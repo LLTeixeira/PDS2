c9b1c9f1833a8ccc13389e4cce0e6e27e75f40fb
André Augusto Moreira Martins <64978311+AndreMartins21@users.noreply.github.com>
2024-06-12 00:07:22 -0300

    feat(rede_social): Cria classe RedeSocial

--- RedeSocial/RedeSocial.cpp
+++ RedeSocial/RedeSocial.cpp
@@ -5,40 +5,64 @@
 #include "Conta.hpp"
 #include "Comentario.hpp"
 
-void PrintContas(std::map <long, Conta*>& Contas) {
 
-	for (auto it = Contas.begin(); it != Contas.end(); ++it) {
-		std::cout << "Key: " << it->first << ", Value: " << (*(it->second)).nome << std::endl;
-	}
+class RedeSocial {
+	private:
+		std::map <long, Conta*> Contas;
 
-}
-
-Post* FindPost(long dono, long id, std::map <long, Conta*>& Contas) {
-	Conta* pConta;
-	auto it = Contas.find(dono); 
-	if (it == Contas.end()) {
-		std::cout << "Account not found" << std::endl;
-		return nullptr;
-	}
-	else {
-		pConta = Contas[dono];
-		if ((*pConta).Mural.size() < id) {
-			std::cout << "Post not found" << std::endl;
-			return nullptr;
+	public:
+		void CriarConta(std::string nome_str) {
+			Conta* conta_ = new Conta(nome_str, Contas);
 		}
-		else {
-			return &(*pConta).Mural[id];
+
+		Conta* GetConta(long id_conta){
+			if (id_conta > Contas.size() || id_conta <= 0){
+				std::cout << "id_conta " << id_conta << " inexistente!" << std::endl;
+				return nullptr;
+			}
+
+			return Contas[id_conta];
 		}
-	}
-}
+
+		void PrintContas() {
+			for (auto it = Contas.begin(); it != Contas.end(); ++it) {
+				std::cout << "Key: " << it->first << ", Value: " << (*(it->second)).nome << std::endl;
+			}
+		}
+
+		Post* FindPost(long dono_id, long id_post) {
+			Conta* pConta;
+			auto it = Contas.find(dono_id); 
+			if (it == Contas.end()) {
+				std::cout << "Account not found" << std::endl;
+				return nullptr;
+			}
+			else {
+				pConta = Contas[dono_id];
+				if ((*pConta).Mural.size() < id_post) {
+					std::cout << "Post not found" << std::endl;
+					return nullptr;
+				}
+				else {
+					return &(*pConta).Mural[id_post];
+				}
+			}
+		}		
+};
+
 
 int main()
 {
-	std::map <long, Conta*> Contas;
+	RedeSocial rede_social = RedeSocial();
+	
+	rede_social.CriarConta("SysAdm");
+	rede_social.CriarConta("GuidoVanRussen");
+	rede_social.PrintContas();
 
-	Conta SysAdm = Conta("SysAdm", Contas);
+	Conta *c1 = rede_social.GetConta(2);
+	std::cout << c1->id << std::endl;
 
-	PrintContas(Contas);
+
 
 	return 0;
 }
